cf5ae3b9a107467ddf8829c33258aad0dcfed7dd
EngelFlores <engel.tads@gmail.com>
2022-08-25 21:11:26 -0300

    Projeto exemplo componente filho

---
+++ src/Link.js
@@ -0,0 +1,10 @@
+export const Link = () => {
+  return (
+    <div>
+      <h2>Titulo 2</h2>
+      <a href="https://www.patasdacasa.com.br/noticia/filhote-de-cachorro-tudo-que-voce-precisa-saber-antes-de-levar-um-caozinho-para-casa_a4379/1">Saber mais</a>
+      <p>Mais texto</p>
+
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import MyApp from './MyApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MyApp></MyApp>
 );
 
 // If you want to start measuring performance in your app, pass a function
